dc7b2633b11eb2baa2cedc008337dbb9528373bc
Brahim Laarif <brahim.laarif@gmail.com>
2018-09-25 09:14:33 +0100

    Add product model

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,9 +1,12 @@
 using Microsoft.EntityFrameworkCore;
+using ShoppingApp.WebAPI.Data.Models;
 
 namespace ShoppingApp.WebAPI.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<Product> Products { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {

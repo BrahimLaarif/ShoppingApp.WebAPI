74787cdf0a56a741d9849943905e39107815576b
Brahim Laarif <brahim.laarif@gmail.com>
2018-09-25 22:57:44 +0100

    Update seeds

--- Data/Seeds/FakeSeed.cs
+++ Data/Seeds/FakeSeed.cs
@@ -16,12 +16,30 @@ namespace ShoppingApp.WebAPI.Data.Seeds
 
         public void Initialize()
         {
+            CategorySeed();
+            ProductSeed();
+        }
+
+        private void CategorySeed()
+        {
             if (!context.Categories.Any())
             {
                 var categoriesJsonData = System.IO.File.ReadAllText("Data/Seeds/MockData/Categories.json");
                 var categories = JsonConvert.DeserializeObject<IEnumerable<Category>>(categoriesJsonData);
 
                 context.Categories.AddRange(categories);
+                context.SaveChanges();
+            }
+        }
+
+        private void ProductSeed()
+        {
+            if (!context.Products.Any())
+            {
+                var productsJsonData = System.IO.File.ReadAllText("Data/Seeds/MockData/Products.json");
+                var products = JsonConvert.DeserializeObject<IEnumerable<Product>>(productsJsonData);
+
+                context.Products.AddRange(products);
                 context.SaveChanges();
             }
         }
--- Entities/Models/Product.cs
+++ Entities/Models/Product.cs
@@ -7,6 +7,9 @@ namespace ShoppingApp.WebAPI.Entities.Models
     {
         public int Id { get; set; }
 
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+
         [Required]
         public string Name { get; set; }
 
@@ -14,8 +17,5 @@ namespace ShoppingApp.WebAPI.Entities.Models
         public string Description { get; set; }
 
         public DateTime DateCreated { get; set; }
-
-        public int CategoryId { get; set; }
-        public virtual Category Category { get; set; }
     }
 }
